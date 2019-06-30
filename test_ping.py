ac1d9985fe942509c0bde483fad2f24b6c2a85f5
soundreaper <soundreaper@gmail.com>
2019-06-30 14:34:23 -0700

    Add Regions/IP/Ping Test
    Add the different regions, their respective IP addresses and a ping test with a region selection

---
+++ test_ping.py
@@ -0,0 +1,12 @@
+from ping3 import ping, verbose_ping
+
+regionIP = [["NA"["104.160.131.3"]],,,,]
+    : ,
+    "EUW": "104.160.141.3",
+    "EUNE": "104.160.142.3",
+    "OCE": "104.160.156.1",
+    "LAN": "104.160.136.3",
+}
+
+print('\nThe current region is ' + regionIP[1] + '\n')
+#verbose_ping(regionIP[1], timeout=10, count=16)

304fad34c7979907748999fae64774450a15950d
soundreaper <soundreaper@gmail.com>
2019-07-07 23:40:09 -0700

    Update dictionary and create select menu
    Create the select menu to select region and add IP's for each region

--- test_ping.py
+++ test_ping.py
@@ -1,12 +1,25 @@
 from ping3 import ping, verbose_ping
 
-regionIP = [["NA"["104.160.131.3"]],,,,]
-    : ,
+regionIP = {
+    "NA": "104.160.131.3",
     "EUW": "104.160.141.3",
     "EUNE": "104.160.142.3",
     "OCE": "104.160.156.1",
     "LAN": "104.160.136.3",
 }
 
-print('\nThe current region is ' + regionIP[1] + '\n')
-#verbose_ping(regionIP[1], timeout=10, count=16)
+while True:
+    choice = input ("Which server would you like?:\n1. NA\n2. EUW\n3. EUNE\n4. OCE\n5. LAN\n\n")
+    if choice in ['1','2','3','4','5']:
+        break
+# process the input
+if choice == '1': 
+     verbose_ping(regionIP.get("NA"), timeout=10, count=16)
+elif choice == '2': 
+    verbose_ping(regionIP.get("EUW"), timeout=10, count=16)
+elif choice == '3': 
+    verbose_ping(regionIP.get("EUNE"), timeout=10, count=16)
+elif choice == '4': 
+    verbose_ping(regionIP.get("OCE"), timeout=10, count=16)
+elif choice == '5': 
+    verbose_ping(regionIP.get("LAN"), timeout=10, count=16)
